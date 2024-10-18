4f239dde25c3ddc778ef903aae703d632842721e
Viktor Wesström <viktor.wesstrom@invencon.se>
2024-10-18 15:47:49 +0200

    multiple streaming charts with freeze btn and total point counter

--- tauri-react-live-charts/src/App.tsx
+++ tauri-react-live-charts/src/App.tsx
@@ -1,14 +1,29 @@
 import { TestCanvas } from './components/ui/TestCanvas';
-import { useDataPoint } from './hooks/useData';
+import Chart from 'chart.js/auto';
+import StreamingPlugin from '@robloche/chartjs-plugin-streaming';
+import { useState } from 'react';
+
+Chart.register(StreamingPlugin);
 
 function App() {
-  return <div className="h-[200px] w-[1000px] bg-slate-300">{<Wrapper />}</div>;
+  return <div className="min-h-full min-w-full bg-slate-900 text-slate-200">{<Wrapper />}</div>;
 }
 
 const Wrapper = () => {
-  const { dataPoint } = useDataPoint();
-
-  return dataPoint && <TestCanvas dataPoint={dataPoint} />;
+  const [freeze, setFreeze] = useState(false);
+  const [count, setCount] = useState(0);
+  const toggleFreeze = () => setFreeze((prev) => !prev);
+  return (
+    <div className="relative flex flex-col items-center justify-between">
+      <button onClick={toggleFreeze}>Freeze</button>
+      <h1>Total points: {count}</h1>
+      {Array.from({ length: 20 }).map((_v, i) => (
+        <div key={i} className="">
+          <TestCanvas freeze={freeze} onAddPoint={() => setCount((prev) => prev + 1)} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- tauri-react-live-charts/src/components/ui/TestCanvas.tsx
+++ tauri-react-live-charts/src/components/ui/TestCanvas.tsx
@@ -1,65 +1,68 @@
-import Chart from 'chart.js/auto';
-import { useEffect, useRef, useState } from 'react';
-import { DataPoint } from './DataPoint';
-import StreamingPlugin from '@robloche/chartjs-plugin-streaming';
+import { Chart, ChartOptions } from 'chart.js';
 import 'chartjs-adapter-luxon';
+import { useRef, useState } from 'react';
+import { Line, getElementAtEvent } from 'react-chartjs-2';
 
-type TestCanvasProps = {
-  dataPoint: DataPoint;
-};
+export const TestCanvas = ({ freeze, onAddPoint }: { freeze: boolean; onAddPoint: () => void }) => {
+  const chartRef = useRef<Chart<'line'>>(null);
 
-Chart.register(StreamingPlugin);
+  const handleClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    console.log(getElementAtEvent(chartRef.current!, event));
+  };
+  const [data] = useState(() => ({
+    labels: [],
+    datasets: [
+      {
+        label: 'dataset',
+        data: [],
+        fill: false,
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+      },
+    ],
+  }));
 
-export const TestCanvas = ({ dataPoint }: TestCanvasProps) => {
-  const [chart, setChart] = useState<Chart>();
-  const canvas = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const context = canvas.current!.getContext('2d');
-    if (context !== null) {
-      const myChart = new Chart(context, {
-        type: 'line',
-        data: {
-          labels: [],
-          datasets: [
-            {
-              label: 'dataset',
-              data: [],
-              fill: false,
-              backgroundColor: 'rgba(75,192,192,0.2)',
-              borderColor: 'rgba(75,192,192,1)',
-            },
-          ],
-        },
-        options: {
-          plugins: {
-            streaming: {
-              duration: 10000,
-              ttl: 100000,
-              refresh: 1000,
-            },
+  const options: ChartOptions<'line'> = {
+    responsive: true,
+    resizeDelay: 500,
+    animation: false,
+    aspectRatio: 6,
+    onResize(chart, size) {
+      console.log(size);
+    },
+    plugins: {
+      streaming: {
+        duration: 600_000,
+        ttl: 600_000,
+        delay: 1000,
+        frameRate: 30,
+        refresh: 250,
+        pause: freeze,
+      },
+    },
+    scales: {
+      x: {
+        type: 'realtime',
+        realtime: {
+          onRefresh: (chart: Chart) => {
+            chart.data.datasets[0].data.push({
+              x: new Date().getTime(),
+              y: Math.random(),
+            });
+            onAddPoint();
           },
-          scales: {
-            x: {
-              type: 'realtime',
-              realtime: {
-                delay: 250,
-                onRefresh: (chart: Chart) => {
-                  console.log(dataPoint);
-                  chart.data.datasets[0].data.push({
-                    y: dataPoint.value,
-                    x: dataPoint.timestamp,
-                  });
-                },
-              },
-            },
-          },
         },
-      });
-      setChart(myChart);
-      return () => myChart.destroy();
-    }
-  }, []);
+      },
+    },
+  };
 
-  return <canvas ref={canvas} width="400" height="400"></canvas>;
+  return (
+    <Line
+      style={{ position: 'relative', height: '40vh', width: '80vw' }}
+      onClick={handleClick}
+      ref={chartRef}
+      data={data}
+      options={options}
+    />
+  );
 };
